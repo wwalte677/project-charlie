f9edb0505cd8651fec45a77e3263ec08620dbc00
Wyatt <wwalte677@gmail.com>
2025-11-24 17:38:51 -0800

    Fixed Routing and Backend Springboot @Beans

--- backend/src/main/java/com/projectcharlie/service/AuditService.java
+++ backend/src/main/java/com/projectcharlie/service/AuditService.java
@@ -1,5 +1,8 @@
 package com.projectcharlie.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuditService {
     
 }
--- backend/src/main/java/com/projectcharlie/service/BallotService.java
+++ backend/src/main/java/com/projectcharlie/service/BallotService.java
@@ -3,10 +3,14 @@ package com.projectcharlie.service;
 import com.projectcharlie.model.Ballot;
 import com.projectcharlie.model.BallotState;
 import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 import java.time.LocalDateTime;
 
+@Service
 public class BallotService {
     
 }
